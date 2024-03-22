76b48958c8ba6889aab4adb53067a8998a1eb5e4
Bruna Varela <brunavarela1@hotmail.com>
2024-03-22 18:48:37 -0300

    :sparkles: feat: add get, post, update, delete users

--- frontend/cadastro-de-pessoas/src/App.jsx
+++ frontend/cadastro-de-pessoas/src/App.jsx
@@ -4,6 +4,8 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Form from "./components/Form";
 import Grid from "./components/Grid";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -18,12 +20,28 @@ const Container = styled.div`
 const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
-        <Form />
-        <Grid />
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit} />
       </Container>
       <ToastContainer autoClose={3000} position="bottom-left" />
       <GlobalStyle />
